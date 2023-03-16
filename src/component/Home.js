b76f356e380596c563d579880dadb90cf423b43f
edmude0767 <85378110+edmude0767@users.noreply.github.com>
2023-03-16 13:53:25 -0500

    Act 160320231500

--- src/component/Home.js
+++ src/component/Home.js
@@ -7,6 +7,7 @@ function Home() {
       
     <><div><h1>Logros y reconocimientos</h1></div><br /><div className="container">
       <br />
+   
       <Card style={{ height: '35rem', width: '35rem', padding: '3', margin: '10', }}>
         <Card.Img variant="top" height='300' width='300' src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgc1EOD0WuTLG_G2UHBgjDkZHOtnlSNSsyiYBCC9PxcSbymGMP2ZMXUc2dw0EODfSAKlyrFjaWfXqBZKqJyy-XTB07hnPOROyRfHpczoTparxhqGnhfzIo65xe5JmS23uZSo1_VvyP8X4iAU4FaXU6y5Dv69HyjkP5X1p4H50LXnu_E9qPTOKvj9EOuDQ/w613-h408/Gestion%20de%20Riesgos.png"/>
         <Card.Body>
@@ -17,11 +18,12 @@ function Home() {
           <Button variant="primary" href="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh7c-o8z22cz97N4ccwIbd-Y0Ki0ktbEnDD0EL95LjwBGO4f5Dqgt5uI0xUpbH0_vLsmzsJ8Plj14kUxqE9dfydbPMb2eUS3pgrQia67_Hh2EqD_qe71UVHDIDjvR3-fCCsY70PKM301JzX2BQpLLU_MpkPbCsQbzwSpvhfcMvmNSTCrtIfIEXmfHJlkQ/w414-h392/Lo%20Sentimos.png">Ampliar informacion</Button>
         </Card.Body>
       </Card>
+    
 
       <Card style={{ height: '35rem', width: '35rem', padding: 3, margin: 10 }}>
         <Card.Img variant="top" height='300' width='300' src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiau59z7Oz37JawaQMpPm-wJIkx78ECUlBMkyRPvEtOm2pBnPfJNOyWKBiDsF94SLXnpTT3TukUB0aJV5ORVL04wm7v9oG_m413VvbdG1ooTpwbq_GRekLBcMR6WguXMvYMpavcwVnogRvbXDOWyq3IJ168zaAzvSMMUFwaFsSRbr6ST-VZO6IWEyyIUw/s1127/Gestion%20de%20Riesgo%20en%20excel.png" />
         <Card.Body>
-          <Card.Title>Desarrollo de un sistema de Gestion de riesgos en Excel</Card.Title>
+          <Card.Title>Sistema de Gestion de riesgos en Excel</Card.Title>
           <Card.Text>
           Desarrollé un App en Excel con el objetivo de Identificar, analizar, valorar y tratar los riesgos de una empresa, Bajo la norma la Norma ISO 31000.
           </Card.Text>
@@ -33,7 +35,7 @@ function Home() {
         
       <Card.Img variant="top" height='300' width='300' src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgOmDhtSNDDOsraBhfyOsD80SDSrT3vhWrTFRQRf6h0phrMvO9uUaH8bbIixRrLkxpGMmRAq0xFP5QaDfhFDXo0ChXD0tlnYRMRoF2B0agwEsb9R4KoLzKmAbrzDDkiYuQzjCLo--BWc9Lnpreka1ayzZPxVL2dLNMctRHo4t2XQV0YYyZHnCjncM6ePg/w642-h485/Diagrama%20de%20Gantt.png" />
         <Card.Body>
-          <Card.Title>Desdarrollo de un Diagrama de Gantt en power BI</Card.Title>
+          <Card.Title>Desarrollo de un Diagrama de Gantt en power BI</Card.Title>
           <Card.Text>
           Dicha herramienta de visualización de datos que se utiliza para mostrar un proyecto o un plan de trabajo en una línea de tiempo. Esta nos permite tener una vista general del progreso del proyecto, identificar las tareas críticas y los retrasos, y ajustar los plazos para cumplir con los objetivos del proyecto.
 
